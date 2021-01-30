4a63a3170310b0cce80e416db1001974e4062f9e
Eko Pebrisulistiyo <eko.pebrisulistiyo@gmail.com>
2021-01-31 01:06:33 +0800

    Update Final

--- web/route.go
+++ web/route.go
@@ -8,23 +8,23 @@ import (
 
 //Route function
 func Route(router *gin.Engine) *gin.Engine {
-	// router.GET("/province", controllers.GetListProvince)
-	// router.GET("/province/:id", controllers.GetProvinceByID)
-	// router.POST("/province", controllers.AddProvince)
-	// router.PUT("/province/:id", controllers.UpdateProvince)
-	// router.DELETE("/province", controllers.DeleteProvince)
+	router.GET("/province", controllers.GetListProvince)
+	router.GET("/province/:id", controllers.GetProvinceByID)
+	router.POST("/province", controllers.AddProvince)
+	router.PUT("/province/:id", controllers.UpdateProvince)
+	router.DELETE("/province", controllers.DeleteProvince)
 
-	// router.GET("/district", controllers.GetListDistrict)
-	// router.GET("/district/:id", controllers.GetDistrictByID)
-	// router.POST("/district", controllers.AddDistrict)
-	// router.PUT("/district/:id", controllers.UpdateDistrict)
-	// router.DELETE("/district", controllers.DeleteDistrict)
+	router.GET("/district", controllers.GetListDistrict)
+	router.GET("/district/:id", controllers.GetDistrictByID)
+	router.POST("/district", controllers.AddDistrict)
+	router.PUT("/district/:id", controllers.UpdateDistrict)
+	router.DELETE("/district", controllers.DeleteDistrict)
 
-	// router.GET("/subdistrict", controllers.GetListSubDistrict)
-	// router.GET("/subdistrict/:id", controllers.GetSubDistrictByID)
-	// router.POST("/subdistrict", controllers.AddSubDistrict)
-	// router.PUT("/subdistrict/:id", controllers.UpdateSubDistrict)
-	// router.DELETE("/subdistrict", controllers.DeleteSubDistrict)
+	router.GET("/subdistrict", controllers.GetListSubDistrict)
+	router.GET("/subdistrict/:id", controllers.GetSubDistrictByID)
+	router.POST("/subdistrict", controllers.AddSubDistrict)
+	router.PUT("/subdistrict/:id", controllers.UpdateSubDistrict)
+	router.DELETE("/subdistrict", controllers.DeleteSubDistrict)
 
 	router.GET("/persons", controllers.GetListPersons)
 	router.GET("/person/:id", controllers.GetPersonByID)
